927bc29c1d2601fd352bf5e9637ecbda800fa967
Nadiein Oleh <olegnadein1994@gmail.com>
2023-12-26 12:19:51 +0200

    env

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['mongoose']
+    serverComponentsExternalPackages: ['mongoose'],
   },
   reactStrictMode: true,
   swcMinify: true,
   modularizeImports: {
     '@mui/icons-material': {
       transform: '@mui/icons-material{{member}}',
-    }
-  }
-}
+    },
+  },
+  env: {
+    MONGO_URL: 'mongodb+srv://olehnadieindev:Pass321@cluster0.x5ql9gk.mongodb.net/catalog?retryWrites=true&w=majority',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
